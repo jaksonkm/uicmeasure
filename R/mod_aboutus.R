5f2b3b688088ae7516cf9fbd9c5a7314fd697738
jaksonkm <111496640+jaksonkm@users.noreply.github.com>
2022-10-20 23:36:29 +1100

    First commit

--- R/mod_aboutus.R
+++ R/mod_aboutus.R
@@ -4,28 +4,32 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_aboutus_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    bs4Card(status="primary",height = 500,collapsible = FALSE,
+            title = h3(strong("About Us")), width =10,h5("We are a group of researchers, who are passionate about university-industry collaboration."),h5(" Contact at: rich.awasthy@canberra.edu.au.",style="text-align: justify;", sep="<br/>")
+
+    )
+
   )
 }
-    
+
 #' aboutus Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_aboutus_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_aboutus_ui("aboutus_1")
-    
+
 ## To be copied in the server
 # mod_aboutus_server("aboutus_1")
